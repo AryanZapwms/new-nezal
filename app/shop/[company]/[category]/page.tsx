718e8125987d127754a55698393b67f6b25595c4
AryanZapwms <aryan@zapwms.com>
2026-05-14 09:33:49 +0530

    build fixed

--- app/shop/[company]/[category]/page.tsx
+++ app/shop/[company]/[category]/page.tsx
@@ -4,7 +4,7 @@
 import React, { useEffect, useMemo, useState } from "react"
 import { useParams } from "next/navigation"
 import Link from "next/link"
-import { ProductCard } from "@/components/product-card"
+import  ProductCard  from "@/components/product-card"
 import { BrandFilters } from "@/components/brand-filters"
 import { Button } from "@/components/ui/button"
 import { Home, ChevronRight, Star, Filter } from "lucide-react"
